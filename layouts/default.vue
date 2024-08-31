168dbe951689bd1a16bc40028143bf2b11020d9e
Ilatian <sebastian.grajales15@gmail.com>
2024-08-31 12:36:13 -0500

    set

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,13 @@
   <div class="container-fluid bg-gray-black">
     <div class="flex overflow-x-hidden">
       <Navbar />
-      <div class="relative flex-none hidden lg:block w-[6%] hover:w-3/12 bg-gradient-to-r  from-transparent  hover:from-black from-15% to-transparent z-[900] transition-all duration-300">
-           <SidebarRounded />
-       </div>
-       <div class="relative flex-grow w-auto transition-all duration-300">
-           <slot></slot>
-       </div>
+      <div
+        class="relative flex-none hidden lg:block w-[6%] hover:w-3/12 bg-gradient-to-r  from-transparent  hover:from-black from-15% to-transparent z-[900] transition-all duration-300">
+        <SidebarRounded />
+      </div>
+      <div class="relative flex-grow w-auto transition-all duration-300">
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +20,6 @@ export default {
   components: {
     SidebarRounded,
     Navbar
-
   }
 };
 </script>
